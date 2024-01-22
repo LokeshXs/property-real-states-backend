4c78cf229670419fceaf49464eb08c1e9e5433f8
LOKESH SINGH <lokesh1129ece@gmail.com>
2024-01-22 16:23:08 +0530

    edited cors options

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 const corsOptions ={
-  origin:['http://localhost:5173','https://real-state-frontend-lokeshxs.vercel.app/'], 
+  origin:['http://localhost:5173','https://real-state-frontend-lokeshxs.vercel.app'], 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
