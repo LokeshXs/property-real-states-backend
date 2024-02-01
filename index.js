3b0f9c9cd006c54ebc732c9fd7dbbc91a751afaf
LOKESH SINGH <lokesh1129ece@gmail.com>
2024-02-01 15:29:37 +0530

    changed the frontenf url in cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 const corsOptions ={
-  origin:['http://localhost:5173',process.env.FRONTEND_URL], 
+  origin:['http://localhost:5173',"https://main--celadon-croissant-c99bf1.netlify.app"], 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
